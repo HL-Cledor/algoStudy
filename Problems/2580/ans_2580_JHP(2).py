de1ded6947ab5156db6449d8776370da23f66585
Bobzzang <jaehyun_22@naver.com>
2022-01-07 21:10:01 +0900

    Daily Commit on 2022.01.07 - Problem 2580 Try(2) with using list remove method : Still runtime error....

--- Problems/2580/ans_2580_JHP(2).py
+++ Problems/2580/ans_2580_JHP(2).py
@@ -7,57 +7,67 @@
 
 import sys
 
-def line_check(x,y):    
-    for i in range(0,9):
-        if i != y:
-            if board[x][i] == board[x][y]:
-                return False
+def num_check(x,y):    
+    defNum= [1,2,3,4,5,6,7,8,9]
     
-    for j in range(0,9):
-        if j != x:
-            if board[j][y] == board[x][y]:
-                return False     
-    return True
+    # (1) 가로/세로 비교
+    for i in range(9):
+        if board[x][i] in defNum:
+            defNum.remove(board[x][i])         
+        if board[i][y] in defNum:
+            defNum.remove(board[i][y])     
     
-def box_check(x,y):
+    # (2) Box 확인 
     n_x = x//3 # 3x3
-    n_y = y//3 # 3x3
-    
-    for i in range(n_x*3, (n_x+1)*3):
-        for j in range(n_y*3, (n_y+1)*3):
-            if board[i][j] == board[x][y]:
-                return False
-    return True
+    n_y = y//3 # 3x3    
+    for j in range(n_x*3, (n_x+1)*3):
+        for k in range(n_y*3, (n_y+1)*3):
+            if board[j][k] in defNum:
+                defNum.remove(board[j][k])
 
-def dfs(x,y):
+    return defNum    
+
+def dfs(x):
     global board
+    global check
+    global flag
     
-    if x == 9:
+    if flag:
+        return
+    
+    if x == len(check):
         for row in board:
-            for val in row:
-                print(val, end=' ')
-            print()
+            print(*row) # 이렇게도 print 가능하더라고요
+        flag = True
+        return
+
     else:
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == 0:
-                    for val in range(1,10):
-                        if line_check(i,j) == True and box_check(i,j) == True:
-                            board[i][j] = val
-                            dfs(x+1,0)
-                            board[i][j] = 0
+        (i,j) = check[x]
+        nums = num_check(i,j)
+        
+        for val in nums:
+            board[i][j] = val
+            dfs(x+1)
+            board[i][j] = 0
 
 def solve():
     global board
+    global check
+    global flag
     
     input = sys.stdin.readline
     board = []
-    defNum= [1,2,3,4,5,6,7,8,9]
+    check = []
+    flag = False
     
     for i in range(9):
-        board.append(list(map(int,input().split())))
+        data = list(map(int,input().split()))
+        board.append(data)
+        for j in range(9):
+            if data[j] == 0:
+                check.append((i,j))   # 0 이 위치한 (x,y) 저장             
     
-    dfs(0,0) # x,y = 스도쿠 Array 위치 정보
+    dfs(0) # x,y = 스도쿠 Array 위치 정보
 
 if __name__ == "__main__":
     solve()
